df9775461e39427b3a9d15d4304110ad3f2ccdc0
Vasiliy <wudgexyz@gmail.com>
2021-06-16 03:02:28 +0300

    initial

---
+++ SA_NetLauncher/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using launcher_template.UI.News;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace launcher_template
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 5; i++)
+                newsPanel.Children.Add(new Additional());
+            
+        }
+
+        private void bClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void bHide_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void bMainSite_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://vk.com/id1");
+        }
+    }
+}
